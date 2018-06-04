6f75f9156f95727f8ee4f04c372b66f2f541bb3a
Peter Kim <peterckim96@gmail.com>
2018-06-04 18:32:07 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
-# code here!
+class School
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def roster
+    roster = {}
+  end
+  
+  def add_student(student, age)
+    roster[student] << age
+  end
+  
+end
