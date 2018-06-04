d150521582c5b40d7c295df37d331011957cc6b7
Peter Kim <peterckim96@gmail.com>
2018-06-04 18:37:38 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,15 +2,16 @@ class School
   
   def initialize(name)
     @name = name
-  end
-  
-  def roster
     @roster = {}
   end
   
+  def roster
+    @roster
+  end
+  
   def add_student(student, age)
-    roster[:age] = []
-    roster[:age] << student
+    @roster[:age] = []
+    @roster[:age] << student
   end
   
 end
